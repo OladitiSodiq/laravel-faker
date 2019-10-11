49ae8d58b8f94699565ffe790b84c1dfc93407b2
OladitiSodiq <Oladitisodiq@gmail.com>
2019-10-11 08:00:16 +0100

    laravel - faker commit git keep dis for me

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\fskes;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Note::class, function (Faker $faker) {
+  return [
+    'title' => $faker->sentence($nbWords = 6, $variableNbWords = true),  // Random task title
+    'description' => $faker->text(), // Random task description
+  ];
+});
